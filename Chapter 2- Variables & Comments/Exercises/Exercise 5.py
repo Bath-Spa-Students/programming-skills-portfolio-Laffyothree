98c54d1e42a9868986b608953a58980201d5be3e
Laffyothree <asfxloveart@gmail.com>
2023-10-18 12:19:16 +0400

    Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 5.py
@@ -0,0 +1,23 @@
+print ("HOW MANY £6 USB STICKS CAN YOU BUY WITH £50?")
+print()
+
+print ("First, how many 6 are there in 50?")
+xmoney = 50
+price = 6
+totalquantity = xmoney // price
+print (f'So 50 divided by 6 is is equals to {totalquantity}')
+
+num1 = 6
+num2 = 8
+ans1 = num1 * num2 
+print (f'If one USB stick costs £6 each, then 8 USB sticks will cost {ans1}')
+
+sol2= "then subtract the £48 cost to the £50"
+xmoney = 50
+totalcost = 48 
+ans2 = xmoney - totalcost
+print (f'{sol2}, and the answer is will be {ans2}')
+print()
+
+print (f"""FINAL ANSWER:
+The girl will then now have a total of {totalquantity} USB sticks with a {ans2} pounds change.""")
