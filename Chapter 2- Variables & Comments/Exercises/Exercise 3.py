014ebf735c49ba2ecb1e2b896900389105beb844
Laffyothree <asfxloveart@gmail.com>
2023-10-18 12:18:59 +0400

    Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 3.py
@@ -0,0 +1,13 @@
+luv = ("\tI love f&f.\n")
+
+print("Unmodified:")
+print(luv)
+
+print("\nUsing lstrip():")
+print(luv.lstrip())
+
+print("\nUsing rstrip():")
+print(luv.rstrip())
+
+print("\nUsing strip():")
+print(luv.strip())
