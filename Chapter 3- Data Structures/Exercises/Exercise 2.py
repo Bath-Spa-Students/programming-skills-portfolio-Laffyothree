302ed7ed299590fd3082d87bfad8bbefe0bd18f8
Laffyothree <asfxloveart@gmail.com>
2023-10-21 21:21:23 +0400

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -0,0 +1,8 @@
+names = ["Maki", "Gab", "Khudaks", "Memen", "Wami"] 
+message = " is one of my childhood friends."
+
+print (names [0] + message)
+print (names [1] + message)
+print (names [2] + message)
+print (names [3] + message)
+print (names [4] + message)
