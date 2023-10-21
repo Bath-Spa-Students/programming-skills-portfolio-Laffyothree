5861d913af25fd1974974760f5f3e12da8792a97
Laffyothree <asfxloveart@gmail.com>
2023-10-21 21:21:36 +0400

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,5 @@
+dream = ["Black Ford Transit", "Harleyson-Davidson Nightster in matt black", "Porsche 911"]
+
+print (f'I would love to own a {dream [0]}, so that I can travel with my whole family.')
+print (f'It must be nice to have a {dream [1]} for long solo night rides.')
+print (f'I hope one day, I would be able to own my dream {dream [2]}.')
