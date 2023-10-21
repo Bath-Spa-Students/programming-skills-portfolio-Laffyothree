925cda078629ee62c030adb970ded8c27bdc9f40
Laffyothree <asfxloveart@gmail.com>
2023-10-21 21:22:12 +0400

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 7.py
@@ -0,0 +1,40 @@
+print ("Actual List:")
+dream = ['JAPAN', 'SOUTH KOREA', 'SWITZERLAND', 'FRANCE', 'SPAIN']
+print (dream)
+print()
+
+print ("Sorted:")
+dream_list1 = ['JAPAN', 'SOUTH KOREA', 'SWITZERLAND', 'FRANCE', 'SPAIN']
+x = sorted(dream_list1)
+print (x)
+print()
+
+print ("Sorted & Reversed:")
+dream_list2 = ['JAPAN', 'SOUTH KOREA', 'SWITZERLAND', 'FRANCE', 'SPAIN']
+x = sorted(dream_list2, reverse=True)
+print (x)
+print()
+
+print ("Reversed:")
+dream_list3 = ['JAPAN', 'SOUTH KOREA', 'SWITZERLAND', 'FRANCE', 'SPAIN']
+dream_list3.reverse()
+print (dream_list3)
+print()
+
+print ("Reversed back:")
+dream_list4 = ['SPAIN', 'FRANCE', 'SWITZERLAND', 'SOUTH KOREA', 'JAPAN']
+dream_list4.reverse()
+print (dream_list4)
+print()
+
+print ("Sort:")
+dream_list5 = ['JAPAN', 'SOUTH KOREA', 'SWITZERLAND', 'FRANCE', 'SPAIN']
+dream_list5.sort()
+print (dream_list5)
+print()
+
+print ("Reverse sort:")
+dream_list6 = ['JAPAN', 'SOUTH KOREA', 'SWITZERLAND', 'FRANCE', 'SPAIN']
+dream_list6.sort()
+dream_list6.reverse()
+print (dream_list6)
