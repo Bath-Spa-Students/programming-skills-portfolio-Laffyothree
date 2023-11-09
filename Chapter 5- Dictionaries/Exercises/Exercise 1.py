82686593f8a782cda7535fb71711aeebc36cd150
Laffyothree <asfxloveart@gmail.com>
2023-11-09 11:28:56 +0400

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+person = {
+  "first name": "Laffy", 
+  "last name": "Othree", 
+  "age": "20",
+  "city": "UAE"
+}
+
+print(f'''{person["first name"]}
+{person["last name"]}
+{person["age"]}
+{person["city"]}''') 
