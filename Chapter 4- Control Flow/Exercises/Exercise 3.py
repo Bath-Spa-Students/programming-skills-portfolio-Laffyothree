403ab58f01c58015db9159e39df73a58d4e2f198
Laffyothree <asfxloveart@gmail.com>
2023-11-09 11:28:23 +0400

    Exercise 3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 3.py
@@ -0,0 +1,14 @@
+alien = input("What color of alien did you shot? ")
+
+if alien == ("Green"):
+    print ("You just earned 5 points!")
+
+elif alien == ("Yellow"):
+    print ("You just earned 10 points!")
+
+elif alien == ("Red"):
+    print ("You just earned 15 points!")
+
+else: 
+    print ("""Sorry, but you didn't earn any points.
+Better luck next time :)""")
