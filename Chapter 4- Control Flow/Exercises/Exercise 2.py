faaa35dfd63d7f54cce14554c9dbea3888996353
Laffyothree <asfxloveart@gmail.com>
2023-11-09 11:28:10 +0400

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+alien = input("What is the color of the alien? ")
+
+if alien == ("Green"):
+    print ("You just earned 5 points for shooting the alien :)")
+
+else:
+    print ("You just earned 10 points!")
