2494c3589b46f70ffdc6ebba5a71db19da298002
Laffyothree <asfxloveart@gmail.com>
2023-11-09 11:28:45 +0400

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 5.py
@@ -0,0 +1,16 @@
+favorite_fruits = ["banana", "grape", "orange"]
+
+if "banana" in favorite_fruits: 
+    print ("I really love bananas!")
+
+if "grape" in favorite_fruits:
+    print ("I love grapes too hehehe :)")
+
+if "orange" in favorite_fruits: 
+    print ("And oranges as well <3")
+
+if "banana" in favorite_fruits:
+    print ("Well, all of them are my favorites though")
+
+if "grape" in favorite_fruits:
+    print ("But grapes are on the top of my list :3")
