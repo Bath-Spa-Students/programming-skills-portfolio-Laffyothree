2071c60c4f4232491a38bdb148f5bf867fdbdeb4
Laffyothree <asfxloveart@gmail.com>
2023-11-09 11:29:05 +0400

    Exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise 2.py
@@ -0,0 +1,10 @@
+glossary = {
+  "List:\n": "is a data structure in Python that is a mutable, or changeable, ordered sequence of elements.", 
+  "F-strings:\n": "provides a concise, readable way to include the value of Python expressions inside strings.", 
+  "Variables:\n": "are simply a containers for storing data values.",
+  "Def:\n": "is short for “define”. It's a keyword that you need to define a function (aka method).",
+  "Concatenating:\n": "is obtaining a new string that contains both of the original strings."
+}
+
+for key, value in glossary.items():
+    print(key, value)
