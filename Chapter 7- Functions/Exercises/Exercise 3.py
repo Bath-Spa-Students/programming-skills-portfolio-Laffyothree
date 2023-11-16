899421e493217d8021818e926d27376809c2162d
Laffyothree <asfxloveart@gmail.com>
2023-11-16 15:31:19 +0400

    Exercise 3.py

---
+++ Chapter 7- Functions/Exercises/Exercise 3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, text):
+    print (f'The size of the shirt that I am making is size {size}.')
+    print (f'And it says "{text}" on the back.')
+
+make_shirt('Large', 'LA')
+make_shirt(text= 'I love Matcha :P', size= 'Medium')
